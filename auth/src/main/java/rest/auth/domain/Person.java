61210a4ba02d5528fe6bb6e93f58bf2b946ddae0
evgenkolesman <glavstroi_e@mail.ru>
2021-10-06 14:51:44 +0300

    0. RESTFull. Описание архитектуры [#6884] corrections
    0. RESTFull. Описание архитектуры [#6884] corrections

--- auth/src/main/java/rest/auth/domain/Person.java
+++ auth/src/main/java/rest/auth/domain/Person.java
@@ -50,8 +50,12 @@ public class Person {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Person person = (Person) o;
         return id == person.id;
     }
